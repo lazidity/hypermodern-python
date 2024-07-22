18b6ed0f70bcc0618e5f3cf7418f7a898a4dd65b
Lazidity <lazidity@gmail.com>
2024-07-22 23:29:21 +0700

    Handle exceptions gracefully

--- src/hypermodern_python/wikipedia.py
+++ src/hypermodern_python/wikipedia.py
@@ -1,9 +1,14 @@
 # src/hypermodern_python/wikipedia.py
 import requests
+import click
 
 API_URL = "https://vi.wikipedia.org/api/rest_v1/page/random/summary"
 
 def random_page():
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        return response.json()
+    try:
+        with requests.get(API_URL) as response:
+            response.raise_for_status()
+            return response.json()
+    except requests.RequestException as error:
+        message = str(error)
+        raise click.ClickException(message)
