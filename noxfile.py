0c3581c12138d8f675484c1dc31fae1421973703
Lazidity <lazidity@gmail.com>
2024-07-23 22:35:24 +0700

    Manage dependencies in Nox sessions with Poetry

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,7 @@
 # noxfile.py
-import nox
 import tempfile
+
+import nox
 
 nox.options.sessions = "lint", "safety", "tests"
 
@@ -17,15 +18,22 @@ def tests(session):
 @nox.session(python=["3.12"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-bugbear", "flake8-import-order")
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
     session.run("flake8", *args)
 
 
 @nox.session(python=["3.12"])
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
+
 
 @nox.session(python=["3.12"])
 def safety(session):
@@ -39,5 +47,18 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--with=dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--requirement={requirements.name}", *args, **kwargs)
