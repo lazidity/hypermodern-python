8d26025cd68dd52991dde2578a9d413d46338f3c
Lazidity <lazidity@gmail.com>
2024-07-23 10:25:34 +0700

    Code linting with Flake8

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,16 @@
 # noxfile.py
 import nox
 
+locations = "src", "tests", "noxfile.py"
+
 @nox.session(python = [ "3.12" ])
 def tests(session):
     args = session.posargs or [ "--cov", "-m", "not e2e" ]
     session.run("poetry", "install", external = True)
     session.run("pytest", *args)
+
+@nox.session(python = [ "3.12" ])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
